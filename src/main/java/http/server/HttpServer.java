4d12cd6d07b60cceb3842522e0e1b57c5dc81119
cs <casofianou@gmail.com>
2024-10-03 14:33:04 +0200

    Respond with 200

--- src/main/java/http/server/HttpServer.java
+++ src/main/java/http/server/HttpServer.java
@@ -3,17 +3,27 @@ package http.server;
 import http.env.Environment;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public final class HttpServer {
 
-    public HttpServer() { }
+    public HttpServer() {
+    }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(Environment.PORT)) {
             serverSocket.setReuseAddress(true);
-            serverSocket.accept();
-            System.out.println("accepted new connection");
+            try (Socket incoming = serverSocket.accept();
+                 OutputStream os = incoming.getOutputStream();
+                 PrintWriter out = new PrintWriter(os, true, StandardCharsets.UTF_8)) {
+                System.out.println("accepted new connection");
+                out.write("HTTP/1.1 200 OK\r\n\r\n");
+                out.flush();
+            }
         } catch (IOException e) {
             System.out.printf("IOException: %s%n", e.getMessage());
         }
